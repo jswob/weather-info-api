cba20e54becd3124fb537e5a5a24ad70503e2637
jacek swoboda <jacswob@gmail.com>
2019-07-28 07:38:31 +0200

    Create user model

---
+++ source/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    localizations: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Localization"
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
