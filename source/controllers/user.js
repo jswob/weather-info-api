7c17a43a38f088fc278431733c669efdd09238f3
jacek swoboda <jacswob@gmail.com>
2019-07-28 13:48:27 +0200

    Hash new users password with bcrypt

--- source/controllers/user.js
+++ source/controllers/user.js
@@ -1,16 +1,20 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 const { validationResult } = require("express-validator");
 
 const errorGenerator = require("../utils/error-generator");
 
 exports.createUser = async (req, res, next) => {
   const errors = validationResult(req);
-  const user = req.body.user;
+  const saltRounds = 15;
+  const userData = req.body.user;
   try {
     if (!errors.isEmpty()) errorGenerator(422, errors);
-    const newUser = new User(user);
-    await newUser.save();
-    res.json({ user: newUser });
+    const hashedPw = await bcrypt.hash(userData.password, saltRounds);
+    userData.password = hashedPw;
+    const user = new User(userData);
+    await user.save();
+    return res.status(201).json({ user: user });
   } catch (err) {
     next(err);
   }
