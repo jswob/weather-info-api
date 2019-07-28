2230086d8d40cb144d63e3159bda60758bc624ff
jacek swoboda <jacswob@gmail.com>
2019-07-28 13:11:49 +0200

    Create error route handler

--- source/server.js
+++ source/server.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 dotenv.config();
+
+app.use("/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorData = [];
+
+  if (errorStatus === 422) {
+    err.data.array().forEach(validationError => {
+      errorData.push({
+        detail: validationError.msg,
+        source: {
+          pointer: "data/attributes/" + validationError.param.split(".")[1]
+        }
+      });
+    });
+  } else {
+    errorData.push({
+      details: "Server don't work"
+    });
+  }
+  res.status(errorStatus).json({ errors: errorData });
+});
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0-zxrzw.mongodb.net/test?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.MONGODB_USER}:${
+      process.env.MONGODB_PASSWORD
+    }@cluster0-zxrzw.mongodb.net/test?retryWrites=true&w=majority`,
     { useNewUrlParser: true }
   )
   .then(() => {
@@ -16,4 +45,4 @@ mongoose
   })
   .catch(error => {
     throw error;
-  })
+  });
